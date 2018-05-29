2d7b04dda0e8dea21af95a0b3b5d1b25296bed58
Josh Mulloy <joshmulloy1@gmail.com>
2018-05-29 14:13:01 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {artist : 'Widespread Panic', song : 'Driving Song'};
+function updatePlaylist(playlist, artist, song) {
+  playlist['Grateful Dead'] = 'Althea'
+  
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+  return playlist
+}
+
+
+ 
